bb7cf5c7a19664fd4a5c361978757c54d57a24b7
chenli1996 <cl5089@nyu.edu>
2024-04-16 15:54:50 -0400

    test 0.16.0

--- test_open3d.py
+++ test_open3d.py
@@ -1,9 +1,17 @@
-import open3d as o3d
 # import open3d as o3d
-print(o3d.__version__)
+# # import open3d as o3d
+# print(o3d.__version__)
 
-vis = o3d.visualization.Visualizer()
+# vis = o3d.visualization.Visualizer()
+# # vis.create_window() # the 0.17.0 version demands create_window() first, otherwise gives segmentation fault. Why?
+# view_ctl = vis.get_view_control()
+# print(view_ctl)
+# cam_pose_ctl = view_ctl.convert_to_pinhole_camera_parameters()
+# assert id(ctr) == id(vis.get_view_control())  # assertion error.
+
+
+import open3d
+vis = open3d.visualization.Visualizer()
 # vis.create_window() # the 0.17.0 version demands create_window() first, otherwise gives segmentation fault. Why?
 ctr = vis.get_view_control() 
-cam_pose_ctl = vis.convert_to_pinhole_camera_parameters()
 assert id(ctr) == id(vis.get_view_control())  # assertion error.
