4d78dc0efe658933760b744b5954e5b0c7221bd0
chenli1996 <cl5089@nyu.edu>
2024-04-30 22:26:27 -0400

    Move folder ./data to ../point_cloud_FoV_Graph

--- comandlines.sh
+++ comandlines.sh
@@ -1,3 +1,6 @@
 # close all python processes except this one
 # kill -9 $(ps aux | grep '[p]ython' | awk '{print $2}')
 kill -9 $(ps | grep '[p]ython' | awk '{print $1}')
+
+# move folder ./data to ../point_cloud_FoV_Graph
+mv ./data ../point_cloud_FoV_Graph
