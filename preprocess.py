bf2da9215334f0070776030b82d2d26122deb21e
chenli1996 <cl5089@nyu.edu>
2024-05-24 16:54:42 -0400

    Refactor code to downsample point cloud data and save as binary PLY files

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,19 @@
-# import open3d as o3d
+import open3d as o3d
 import os
-# from point_cloud_FoV_utils import get_pcd_data_original
+from point_cloud_FoV_utils import get_pcd_data_original
 from tqdm import tqdm
 import pandas as pd
 
-# def downsample_binary_pcd_data():
-#     # Downsample original pcd and save to the binary pcd data
-#     for point_cloud_name in ['longdress','loot','redandblack','soldier']:
-#         if not os.path.exists(f'./data/{point_cloud_name}'):
-#             os.makedirs(f'./data/{point_cloud_name}')
-#         for trajectory_index in tqdm(range(0, 151)):
-#             pcd = get_pcd_data_original(point_cloud_name, trajectory_index)
-#             pcd = pcd.voxel_down_sample(voxel_size=8)
-#             o3d.io.write_point_cloud(f'./data/{point_cloud_name}/frame{trajectory_index}_downsampled.ply', pcd, write_ascii=False)
-#     return pcd
+def downsample_binary_pcd_data():
+    # Downsample original pcd and save to the binary pcd data
+    for point_cloud_name in ['longdress','loot','redandblack','soldier']:
+        if not os.path.exists(f'./data/{point_cloud_name}'):
+            os.makedirs(f'./data/{point_cloud_name}')
+        for trajectory_index in tqdm(range(0, 151)):
+            pcd = get_pcd_data_original(point_cloud_name, trajectory_index)
+            pcd = pcd.voxel_down_sample(voxel_size=8)
+            o3d.io.write_point_cloud(f'./data/{point_cloud_name}/frame{trajectory_index}_downsampled.ply', pcd, write_ascii=False)
+    return pcd
 
 def change2num_points_from_percentage_TLR():
     # read data
